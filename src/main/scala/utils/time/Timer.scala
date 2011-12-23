1c38a5e96081d0f6ceb58bcf148e32e44ad8db37
Geet Duggal <geetduggal@gmail.com>
2011-12-23 18:32:39 -0500

    Timer class added

---
+++ src/main/scala/utils/time/Timer.scala
@@ -0,0 +1,24 @@
+package scalaa.utils.time
+
+/** Class that provides the ability to simply measure the elapsed time
+  * Author: Rob Patro */
+class Timer {
+  private var _tstart = 0L
+  private var _telapsed = 0L
+
+  /** Start the timer */
+  def start = { _tstart = System.currentTimeMillis }
+  
+  /** Stop the timer */
+  def stop = { 
+    val e = if(_tstart > 0L) { System.currentTimeMillis - _tstart } else { 0L } 
+    _telapsed += e
+  }
+  
+  /** Reset the timer */
+  def reset = { _telapsed = 0L; _tstart = 0L }
+  
+  /** Return the amount of time that has elapsed in milliseconds since the
+    * timer started */
+  def elapsed = _telapsed
+}
